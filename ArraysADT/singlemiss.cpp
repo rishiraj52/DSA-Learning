00c7766ae5ed6132950801fb809731c8c155f229
Rishiraj <rajrishi0509@gmail.com>
2021-11-02 20:40:50 +0530

    Arrays file added

---
+++ ArraysADT/singlemiss.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n-1; i++){
+        cin>>arr[i];
+    }
+    
+    if(arr[0]==1){
+        int sum=0;
+        for(int i=0; i<n-1; i++){
+            sum=sum+arr[i];
+        }
+        int s=n*(n+1)/2;
+        cout<<"Missing number = "<<s-sum<<endl;
+    }
+    else{
+        int diff=0-arr[0];
+        for(int i=0; i<n-1; i++){
+            if((i-arr[i])!=diff){
+                cout<<i+diff;
+            }
+        }
+    }
+      
+}
