7be49fa34869c858cf6dae01880e87b831cf56af
Rishiraj <rajrishi0509@gmail.com>
2021-11-05 17:58:39 +0530

    Finding duplicate

---
+++ ArraysADT/findingduplicate.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+// hashing is not implemented properly just a structural code.
+int main() {
+    int arr[10]={2,5,5,7,8,12,12,12,15,17};
+    
+    int lastduplicate=0;
+
+    for(int i=0; i<10; i++){
+        if(arr[i]==arr[i+1] && arr[i]!=lastduplicate){
+            cout<<"Duplicate is "<<arr[i]<<endl;
+            lastduplicate=arr[i];
+        }
+    }
+}
