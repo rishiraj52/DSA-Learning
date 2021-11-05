bd5ada4709492f3d533f2731b2619acad3aaa814
Rishiraj <rajrishi0509@gmail.com>
2021-11-05 13:39:15 +0530

    Single missing element

--- ArraysADT/singlemiss.cpp
+++ ArraysADT/singlemiss.cpp
@@ -4,29 +4,36 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0; i<n-1; i++){
-        cin>>arr[i];
+    for (int i = 0; i < n - 1; i++)
+    {
+        cin >> arr[i];
     }
-    
-    if(arr[0]==1){
-        int sum=0;
-        for(int i=0; i<n-1; i++){
-            sum=sum+arr[i];
+
+    if (arr[0] == 1)
+    {
+        int sum = 0;
+        for (int i = 0; i < n - 1; i++)
+        {
+            sum = sum + arr[i];
         }
-        int s=n*(n+1)/2;
-        cout<<"Missing number = "<<s-sum<<endl;
+        int s = n * (n + 1) / 2;
+        cout << "Missing number = " << s - sum << endl;
     }
-    // else{
-    //     int diff=0-arr[0];
-    //     for(int i=0; i<n-1; i++){
-    //         if((i-arr[i])!=diff){
-    //             cout<<i+diff;
-    //         }
-    //     }
-    // }
-      
+    else
+    {
+        int diff = arr[0] - 0;
+        for (int i = 0; i < n - 1; i++)
+        {
+            if ((arr[i] - i) != diff)
+            {
+                cout << "Missing element= " << i + diff;
+                break;
+            }
+        }
+    }
 }
