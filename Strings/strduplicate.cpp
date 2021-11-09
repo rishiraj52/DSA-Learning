79fe613eee96720b439a63f5ad3319d3d315bbbd
Rishiraj <rajrishi0509@gmail.com>
2021-11-09 17:35:27 +0530

    Duplicate string

---
+++ Strings/strduplicate.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    char A[] = "illusion";
+    long int H = 0, x = 0;
+    int i;
+    for (i = 0; A[i] != '\0'; i++)
+    {
+        x = 1;
+        x = x << (A[i] - 97);
+        if ((x & H) > 0)
+        {
+            cout << A[i] << " is duplicate." << endl;
+        }
+        else
+        {
+            H = H | x;
+        }
+    }
+}
+
+/*
+Bitwise oprations
+
+1. Left Shift <<
+2. Bit ORing (Merging)
+3. Bir ANDing (Masking)
