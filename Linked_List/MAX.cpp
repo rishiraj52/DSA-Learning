f661e8bc4ad2f4ccef89561742579ad441aa4d3b
rishiraj52 <rajrishi0509@gmail.com>
2021-11-30 00:19:09 +0530

    Adding List

---
+++ Linked_List/MAX.cpp
@@ -0,0 +1,86 @@
+#include <cstdio>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} * first;
+
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (int i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Rdisplay(struct Node *p)
+{
+    if (p != 0)
+    {
+        printf("%d ", p->data);
+        Rdisplay(p->next);
+    }
+}
+
+int MAX(struct Node *p)
+{
+    int max = INT32_MIN;
+    while (p != 0)
+    {
+        if (p->data > max)
+        {
+            max = p->data;
+        }
+
+        p = p->next;
+    }
+    return max;
+}
+
+int RMax(struct Node *p)
+{
+    int x = 0;
+    if (p == 0)
+    {
+        return INT16_MIN;
+    }
+    x = RMax(p->next);
+    if (x > p->data)
+    {
+        return x;
+    }
+    else
+    {
+        return p->data;
+    }
+}
+
+int main()
+{
+    int arr[] = {2, 5, 7, 10, 13, 8, 31, 3, 19};
+
+    create(arr, 9);
+    Rdisplay(first);
+    cout << endl;
+    cout << MAX(first) << endl;
+    cout << RMax(first);
+
+    return 0;
+}
