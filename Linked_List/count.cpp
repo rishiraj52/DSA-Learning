0e0bf159b1d6f7a34a025d1e943c1108d647c32b
rishiraj52 <rajrishi0509@gmail.com>
2021-11-29 23:23:59 +0530

    Count List

---
+++ Linked_List/count.cpp
@@ -0,0 +1,76 @@
+#include <cstdio>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} * first;
+
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (int i = 0; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Rdisplay(struct Node *p)
+{
+    if (p != 0)
+    {
+        printf("%d ", p->data);
+        Rdisplay(p->next);
+    }
+}
+
+int count(struct Node *p)
+{
+    int c = 0;
+    while (p != 0)
+    {
+        c++;
+        p = p->next;
+    }
+    return c;
+}
+
+int Rcount(struct Node *p)
+{
+    if (p == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return Rcount(p->next) + 1;
+    }
+}
+
+int main()
+{
+    int arr[] = {2, 5, 7, 10, 13};
+
+    create(arr, 5);
+    Rdisplay(first);
+    cout << endl;
+    cout << count(first) << endl;
+    cout << Rcount(first);
+
+    return 0;
+}
